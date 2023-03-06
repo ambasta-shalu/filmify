e1a52ebd28ac66b198d3a9d011c275f6f6ecd14a
Shalu Ambasta <itsambastakashish@gmail.com>
2023-03-06 22:02:14 +0530

    issuses resolved :)

--- src/components/ErrorCapture.jsx
+++ src/components/ErrorCapture.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-import dogPic from "../assests/Dog.jpg";
 
 function ErrorCapture() {
   return (
-    <div className="error-c container">
-      <div className="error-c-content">
-        <h1 className="error-c-heading">Landed Nowhere?</h1>
-        <p className="error-c-text">
-          <strong>Films Not Found</strong>
-        </p>
-        <p className="error-c-text">This is not where I want to be 😑.</p>
-        <div>
-          <a className="error-c-btn" href="#">
-            <span className="error-c-btn-text nowrap">Get me outta here</span>
-          </a>
-        </div>
+    <div className="error-c">
+      <h1 className="error-c-heading">Landed Nowhere?</h1>
+      <p className="error-c-text">
+        <strong>Films Not Found</strong>
+      </p>
+      <p className="error-c-text">This is not where I want to be 😑.</p>
+      <div>
+        <a className="error-c-btn" href="#">
+          <span className="error-c-btn-text nowrap">Get me outta here</span>
+        </a>
       </div>
-      <img className="error-c-img" src={dogPic} alt="Error Dog Pic" />
     </div>
   );
 }
